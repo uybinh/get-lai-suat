6de82193ca9a8295ef6505a910ab297c8abb6154
Uy Binh <uybinh@gmail.com>
2018-09-13 00:04:51 +0700

    Add save to CSV

--- main.js
+++ main.js
@@ -23,8 +23,17 @@ fetchDOM(url)
     Array.prototype.slice.call(document.querySelectorAll("tr"), 3)
   )
   .then(tablerows => tablerows.map(tablerow => getRateData(tablerow)))
-  .then(data => saveFileAsJson(data))
+  .then(data => {
+    saveFileAsCsv(data)
+    saveFileAsJson(data)
+  })
 
+/**
+ *
+ *
+ * @param {NodeList} tr
+ * @returns json object
+ */
 function getRateData(tr) {
   const rateData = { bank: "", rates: [] }
   Array.prototype.slice.call(tr.children).forEach((child, index) => {
@@ -47,10 +56,37 @@ function saveFileAsJson(data) {
   const fileName = `rates-${date}.json`
   fs.writeFile(fileName, JSON.stringify(data), "utf8", err => {
     if (err) throw err
-    console.log("The file has been saved!")
+    console.log("The file has been saved as JSON!")
+  })
+}
+
+/**
+ *
+ *
+ * @param {JSON Object} data
+ */
+function saveFileAsCsv(data) {
+  const date = formatDate(new Date())
+  const fileName = `rates-${date}.csv`
+  const content = jsonToCsv(data)
+  fs.writeFile(fileName, content, "utf8", err => {
+    if (err) throw err
+    console.log("The file has been saved as CSV!")
   })
 }
 
 function formatDate(date) {
   return `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`
+}
+
+function jsonToCsv(json) {
+  let header =
+    "Bank,KKH,1 month,2 month,3 month,6 month,9 month,12 month,18 month, 24 month,36 month"
+  return (
+    header +
+    "\n" +
+    json
+      .map(rateData => rateData.bank + "," + rateData.rates.join(","))
+      .join("\n")
+  )
 }
